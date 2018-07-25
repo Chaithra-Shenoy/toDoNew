4925ca0eabfab14d5332df3ee3bb9065ddad7d91
Chaithra-Shenoy <chaithrashenoy08@gmail.com>
2018-07-25 19:40:48 +0530

    User Service commited

---
+++ ToDoApplication/src/main/java/com/bridgeit/todoapplication/configuration/ToDoConfig.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.bridgeit.todoapplication.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * 
+ * 
+ * @author Chaithra-Shenoy
+ * @since Date 18-07-2018 <br>
+ *        <p>
+ *        <b>ToDoConfiguration class.</b>
+ *        </p>
+ */
+@Configuration
+public class ToDoConfig {
+
+	@Bean
+	public ModelMapper model() {
+		return new ModelMapper();
+	}
+	/**
+	 * @return PasswordEncoder
+	 *         <p>
+	 *         Method to BCrypt the password
+	 *         </p>
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+
+	}
+}
